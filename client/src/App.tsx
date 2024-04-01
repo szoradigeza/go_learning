a9099e753742b0515d8b2e71ceaf2db5a7d352f9
Geza Szoradi <geza.szilard@vodafone.com>
2024-04-01 18:24:38 +0200

    Add form endpoint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,23 @@
-import { createSignal, type Component } from "solid-js";
-import { createStore } from "solid-js/store";
+import { createResource, createSignal, type Component } from "solid-js";
 
-import { createReconnectingWS, WSMessage } from "@solid-primitives/websocket";
+import { createReconnectingWS } from "@solid-primitives/websocket";
 
-import logo from "./logo.svg";
 import styles from "./App.module.css";
+
+const fetchFormData = async () => {
+  const res = await fetch("http://localhost:8080/form/1");
+  return res.json();
+};
 
 const App: Component = () => {
   const user = Math.random().toString().substr(2, 6);
   const ws = createReconnectingWS("ws://localhost:8080/ws");
-  const [data, setData] = createSignal("");
+  const [data, { mutate }] = createResource(fetchFormData);
 
   ws.addEventListener("message", (ev) => {
+    console.log(data());
     console.log(ev.data);
-    setData(JSON.parse(ev.data).value);
+    mutate(JSON.parse(ev.data));
   });
 
   return (
@@ -22,7 +26,7 @@ const App: Component = () => {
       <input
         id="testInput"
         type="text"
-        value={data()}
+        value={data()?.value || ""}
         onInput={(ev) =>
           ws.send(JSON.stringify({ user, value: ev.currentTarget.value }))
         }
