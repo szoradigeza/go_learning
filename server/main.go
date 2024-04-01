9da936165ac84c04ec47f35444f99e5c34f13d53
Geza Szoradi <geza.szilard@vodafone.com>
2024-04-01 18:24:23 +0200

    Add form endpoint

--- server/main.go
+++ server/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	//"fmt"
+
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
 
@@ -19,50 +23,76 @@ var upgrader = websocket.Upgrader{
 }
 
 type Server struct {
-	conns map[*websocket.Conn]bool
+	conns map[*websocket.Conn]string
 }
 
 func newServer() *Server {
 	return &Server{
-		conns: make(map[*websocket.Conn]bool),
+		conns: make(map[*websocket.Conn]string),
 	}
 }
 
+func generateClientID() string {
+	uuid := uuid.New()
+	return uuid.String()
+}
+
+func (s *Server) handleClose(connToRemove *websocket.Conn) {
+	connToRemove.Close()
+	delete(s.conns, connToRemove)
+	fmt.Println(s.conns)
+}
+
+func getFormData() []FormMock {
+	mockData, _ := os.ReadFile("form.json")
+	var data []FormMock
+	err := json.Unmarshal(mockData, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
+
 func (s *Server) handleWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
 	conn, err := upgrader.Upgrade(w, r, nil)
-	s.conns[conn] = true
+
+	s.conns[conn] = generateClientID()
 
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	defer conn.Close()
+	defer s.handleClose(conn)
 
 	for {
-		_, msg, err := conn.ReadMessage()
+		broadcastMessage := BroadcastMessage{}
+		err := conn.ReadJSON(&broadcastMessage)
 		if err != nil {
 			log.Println(err)
 			break
 		}
 
-		log.Printf("Received: %s\n", msg)
-
-		data := BroadcastMessage{}
-		json.Unmarshal([]byte(msg), &data)
-
-		broadcastMessage := BroadcastMessage{}
-		parseError := json.Unmarshal([]byte(msg), &broadcastMessage)
-
-		if parseError != nil {
-			log.Println(parseError)
-		}
-
-		log.Println(broadcastMessage)
+		log.Printf("Received: %s\n", broadcastMessage)
 
 		s.broadcast(broadcastMessage, conn)
 
 	}
+}
+
+func handleGetForm(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	data := getFormData()[0]
+
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write(jsonBytes)
 }
 
 type BroadcastMessage struct {
@@ -71,23 +101,25 @@ type BroadcastMessage struct {
 }
 
 func (s *Server) broadcast(broadcastMessage BroadcastMessage, exception *websocket.Conn) {
-	byteSlice, err := json.Marshal(broadcastMessage)
-	if err != nil {
-		log.Println("Error:", err)
-		return
-	}
-
 	for conn := range s.conns {
 		if conn == exception {
 			continue
 		}
-		conn.WriteMessage(websocket.TextMessage, []byte(byteSlice))
+
+		conn.WriteJSON(broadcastMessage)
 	}
+}
+
+type FormMock struct {
+	Status string `json:"status"`
+	Value  string `json:"value"`
 }
 
 func main() {
 	flag.Parse()
 	server := newServer()
 	http.HandleFunc("/ws", server.handleWs)
+	http.HandleFunc("GET /form/{id}", handleGetForm)
+
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
